16a73b6a1426ed0928ab6788f655cbd21e0c9ff7
DfrtJulien <de.freitas.julien78@gmail.com>
2024-06-17 11:10:06 +0200

    premier exercice

--- js/bonjour.js
+++ js/bonjour.js
@@ -3,6 +3,12 @@ console.log("test");
 let nom;
 let prenom;
 
-nom = prompt("Quel est votre nom?")
+nom = prompt("Quel est votre nom?");
+prenom = prompt("Quel est votre prénom?")
 
 console.log(nom);
+console.log(prenom);
+
+console.log("votre nom est ", prenom, nom);
+
+alert("votre nom est",nom,prenom);
