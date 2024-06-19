61777ae4448c6c8fd63c40f96a4c07d3dadfe998
DfrtJulien <de.freitas.julien78@gmail.com>
2024-06-19 09:56:01 +0200

    correction des quatres premier éxercices

---
+++ js/valeurs.js
@@ -0,0 +1,19 @@
+console.log("test");
+
+let a = 2;
+a = a - 1;
+a++;
+console.log(a);
+let b = 8;
+b += 2;
+console.log(b);
+let c = a + b * b;
+console.log(c);
+let d = a * b + b;
+console.log(d);
+let e = a * (b + b);
+console.log(e);
+let f = a * b / a;
+console.log(f);
+let g = b / a * a;
+console.log(g);
