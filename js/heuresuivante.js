271c299e16717e72b50739a39810fa30c9a40ac2
DfrtJulien <de.freitas.julien78@gmail.com>
2024-06-25 15:54:41 +0200

    deuxieme jours d'exercice 25/06

--- js/heuresuivante.js
+++ js/heuresuivante.js
@@ -1,28 +1,44 @@
 let hours = parseInt(prompt("Entrez une heure"));
 
 
-if(hours > 0 && hours < 25){
-    let min = parseInt(prompt("Entrez une minute"));
+if(hours >= 0 && hours < 25){
+    let min = parseInt(prompt("Entrez les minute"));
     let seconds = parseInt(prompt("Entrez les secondes"));
     if(seconds === 59){
-        seconds = 0;
-        min++;
-        if(min === 60){
-            min = 0;
-            hours++;
-            if(hours === 24){
-                hours = 0;
-            }
+    seconds = 0;
+    min++;
+    if(min === 60){
+        min = 0;
+        hours++;
+        if(hours === 24){
+            hours = 0;
         }
+    }
+    }else if (seconds > 59){
+        alert('Entrez correctement les seconde')
     }else {
         seconds++;
     }
-    document.write("<p class='text'>" + "il est " + hours + " h " + min +" m " + seconds + " s" + "</p>");
+    document.write("<p class='text'>" + "la réponse est " + hours + " h " + min +" m " + seconds + " s" + "</p>");
 }else{
     alert('Entrez une heure valide');
 }
 
 
 
-
+// if(seconds === 59){
+//     seconds = 0;
+//     min++;
+//     if(min === 60 && min < 60){
+//         min = 0;
+//         hours++;
+//         if(hours === 24){
+//             hours = 0;
+//         }
+//     }
+// }else if (seconds > 59){
+//     alert('Entrez correctement les seconde')
+// }else {
+//     seconds++;
+// }
 
